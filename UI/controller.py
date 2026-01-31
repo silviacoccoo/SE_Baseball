28fe3e19731571406e8bd7523efa03bfca7e907b
silviacoccoo <silvia.cocco04@gmail.com>
2026-01-31 11:46:28 +0100

    punto 1 e 2 terminati

--- UI/controller.py
+++ UI/controller.py
@@ -28,7 +28,12 @@ class Controller:
             self._view.show_alert('Selezionare una squadra!')
             return
 
-        oggetto_squadra=self._model.id_map[int(id_squadra)]
+        oggetto_squadra=self._model.id_map[id_squadra]
+
+        if not self._model.G.has_node(oggetto_squadra):
+            self._view.show_alert('La squadra non fa parte del grafo!')
+            return
+
         vicini=self._model.get_neighbors_team(oggetto_squadra)
 
         self._view.txt_risultato.controls.clear()
@@ -42,6 +47,43 @@ class Controller:
     # TERZO BOTTONE --> CALCOLO DEL PERCORSO
     def handle_percorso(self, e):
         """ Handler per gestire il problema ricorsivo di ricerca del percorso """""
+        id_squadra = self._view.dd_squadra.value
+        if id_squadra is None:
+            self._view.show_alert('Selezionare una squadra!')
+            return
+
+        try:
+            nodo_partenza = self._model.id_map[int(id_squadra)]
+        except KeyError:
+            self._view.show_alert('Squadra non trovata!')
+            return
+
+        if not self._model.G.has_node(nodo_partenza):
+            self._view.show_alert('La squadra selezionata non è nel grafo corrente')
+            return
+
+        best_path, best_score=self._model.get_best_solution(nodo_partenza)
+
+        self._view.txt_risultato.controls.clear()
+        self._view.txt_risultato.controls.append(
+            ft.Text(f'Percorso ottimo trovato con peso totale: {best_score}')
+        )
+
+        self._view.txt_risultato.controls.append(
+            ft.Text(f'Lunghezza del percorso: {len(best_path)} nodi')
+        )
+
+        # Stampo la lista dei nodi
+        percorso_str = ""
+        for i, nodo in enumerate(best_path):
+            percorso_str += f"{nodo.team_code}"
+            if i < len(best_path) - 1:
+                # Aggiungo una freccia e il peso dell'arco per chiarezza
+                peso = self._model.G[nodo][best_path[i + 1]]['weight']
+                percorso_str += f" --({peso})--> "
+
+        self._view.txt_risultato.controls.append(ft.Text(percorso_str))
+
         self._view.update()
         # TODO
 
@@ -51,7 +93,7 @@ class Controller:
         self._view.dd_anno.options.clear()
         self._view.dd_anno.value=None
 
-        all_years=self._model.get_years()
+        all_years=self._model.get_years() # LISTA DI INTERI
 
         for y in all_years:
             option=ft.dropdown.Option(text=y)
@@ -60,7 +102,7 @@ class Controller:
         self._view.dd_anno.update()
 
         # POTREBBE ESSERE UNA RIPETIZIONE - in quanto già fatto nel view
-        self._view.dd_anno.on_change=self.get_selected_year
+        #self._view.dd_anno.on_change=self.get_selected_year
 
     def get_selected_year(self,e):
         """ Handler per gestire la selezione dell'album dal dropdown """""
@@ -72,7 +114,7 @@ class Controller:
             return
 
         # VOGLIO LE SQUADRE DI QUELL'ANNO
-        teams=self._model.get_teams_of_year(year_scelto)
+        teams=self._model.get_teams_of_year(int(year_scelto))
 
         # Una volta selezionato stampare numero squadre ed elenco rispettive sigle
         self._view.txt_out_squadre.controls.clear()
@@ -93,7 +135,7 @@ class Controller:
         self._view.dd_squadra.options.clear()
         self._view.dd_squadra.value = None
 
-        all_teams = self._model.get_teams_of_year(self._view.dd_anno.value)
+        all_teams = self._model.get_teams_of_year(int(self._view.dd_anno.value))
 
         for t in all_teams:
             option = ft.dropdown.Option(key=t.id,
--- model/model.py
+++ model/model.py
@@ -8,6 +8,9 @@ class Model:
         self.id_map={}
         self._lista_teams=[]
 
+        self._best_path=[]
+        self._best_score=0
+
     @staticmethod
     # Se sottolinea in giallo, trasformalo in static method
     def get_years():
@@ -22,19 +25,23 @@ class Model:
         return self._lista_teams # LISTA DI OGGETTI
 
     def build_graph(self,year):
+
         self.G.clear()
 
         self.get_teams_of_year(year)
-        # self.G.add_nodes_from(self._lista_teams)
 
-        # AGGIUNTA ARCHI
         salari_map=DAO.get_teams_salaries(year)
 
+        # MODO 1: CON LA FUNZIONE COMPLETE_GRAPH
         self.G = nx.complete_graph(self._lista_teams)
 
         for u, v in self.G.edges():
             peso = salari_map.get(u.id, 0) + salari_map.get(v.id, 0)
             self.G[u][v]['weight'] = peso
+
+        # MODO 2: CON IL CICLO
+
+        # self.G.add_nodes_from(self._lista_teams)
 
         """
         for i in range(len(self._lista_teams)):
@@ -53,6 +60,64 @@ class Model:
             peso=self.G[team][v]['weight']
             risultato.append((v,peso))
 
-        risultato.sort(key=lambda x: x[1], reverse=True)
+        risultato.sort(key=lambda x: x[1], reverse=True) # x[1] indica di ordinare in base al peso che si trova in posizione 1 della tupla
         return risultato
+
+    # PUNTO 2 RICORSIONE
+    def get_best_solution(self,nodo_partenza):
+        """Prepara i dati e lancia la ricorsione"""
+        # RESET
+        self._best_path=[]
+        self._best_score = 0
+
+        # RECUPERO DATI NECESSARI
+        parziale=[nodo_partenza]
+
+        self.ricorsione(parziale,float('inf'))
+        return self._best_path,self._best_score
+
+    def ricorsione(self,parziale,ultimo_peso):
+        # Calcolo il peso attuale del percorso parziale
+        peso_corrente=self.calcola_peso_percorso(parziale)
+
+        # Caso terminale
+        if peso_corrente > self._best_score:
+            self._best_score=peso_corrente
+            self._best_path=list(parziale)
+
+        # Caso ricorsivo
+        ultimo_nodo=parziale[-1]
+        vicini_possibili=[]
+
+        for vicino in self.G.neighbors(ultimo_nodo):
+            peso_arco=self.G[ultimo_nodo][vicino]['weight']
+
+            # VINCOLI
+            # Non devo aver visitato il nodo
+            if vicino not in parziale:
+                # Il peso deve essere strettamente decrescente
+                if peso_arco<ultimo_peso:
+                    vicini_possibili.append((vicino, peso_arco))
+        # Ordino per peso decrescente
+        vicini_possibili.sort(key=lambda x: x[1], reverse=True)
+
+        K=3
+        vicini_migliori=vicini_possibili[:K]
+
+        for v,p in vicini_migliori:
+            parziale.append(v)
+            self.ricorsione(parziale,p)
+            parziale.pop()
+
+    def calcola_peso_percorso(self, lista_nodi):
+        if len(lista_nodi)<2:
+            return 0
+        peso_tot=0
+        for i in range(len(lista_nodi)-1):
+            u=lista_nodi[i]
+            v=lista_nodi[i+1]
+            peso_tot+=self.G[u][v]['weight']
+        return peso_tot
+
+
 
